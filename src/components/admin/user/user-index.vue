6f02bc2b2ad226be65ee018ff5bdd4d6833adeee
kenzyyang <827506072@qq.com>
2019-04-22 16:12:36 +0800

    登录请求顺序问题解决

--- src/components/admin/user/user-index.vue
+++ src/components/admin/user/user-index.vue
@@ -5,6 +5,7 @@
         </div>
         <div class="table" style="width: 100%;">
             <el-table
+                    v-loading="tableLoading"
                     :data="tableData"
                     height="500"
                     border
@@ -57,30 +58,64 @@
                 currentPage: 1,
                 total: 100,
                 tableData: [
-                    {
-                        userName: 'kenzyyang',
-                        nickName: '123',
-                        email: '827506072@qq.com',
-                        role: 2
-                    }
-                ]
+                    // {
+                    //     userName: 'kenzyyang',
+                    //     nickName: '123',
+                    //     email: '827506072@qq.com',
+                    //     role: 2
+                    // }
+                ],
+                tableLoading: false,
+                isInit: false
             }
         },
         methods: {
-            updateTable(){
+            updateTable() {
+                this.tableLoading = true;
                 let data = {
                     currentPage: 1,
                     currentSize: 10
                 };
-                getAllUser(data).then((response)=>{
-                    console.log(response);
-                }).catch((err)=>{
-
+                getAllUser(data).then((response) => {
+                    if (response.status === 200 && response.data.code === 0) {
+                        const result = response.data.data;
+                        let tableData = [];
+                        this.total = result.count;
+                        for (let i in result.list) {
+                            tableData.push(result.list[i]);
+                        }
+                        this.tableData.splice(0);
+                        this.tableData.push(...tableData);
+                    }
+                }).catch((err) => {
+                    console.log('请求用户信息失败: ', err);
+                }).finally(() => {
+                    this.tableLoading = false;
                 });
             }
         },
+        computed: {
+            userLogin() {
+                return this.$store.state.user.isLogin;
+            },
+            userLoaded() {
+                return this.$store.state.user.isLoaded;
+            }
+        },
+        watch: {
+            userLoaded: {
+                handler(val) {
+                    if (val && this.userLogin &&  !this.isInit) {
+                        this.updateTable();
+                    }
+                }
+            }
+        },
         mounted() {
-            this.updateTable();
+            if (this.userLoaded && this.userLogin) {
+                this.isInit = true;
+                this.updateTable();
+            }
         }
     }
 </script>
@@ -101,7 +136,8 @@
         .table {
             margin-top: 10px;
         }
-        .pagination{
+
+        .pagination {
             margin-top: 10px;
             width: 100%;
             height: 30px;
